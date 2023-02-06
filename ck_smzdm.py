15ff4db305689dd253be159d0e64b9de6610dcdd
Pwolf-Lin <276104733@qq.com>
2023-02-06 17:34:03 +0800

    更新

--- ck_smzdm.py
+++ ck_smzdm.py
@@ -1,12 +1,14 @@
 # -*- coding: utf-8 -*-
 """
 cron: 51 9 * * *
-new Env('什么值得买');
+new Env('什么值得买APP');
 """
 
-from urllib.parse import quote, unquote
 
 import requests
+import json
+import time
+import hashlib
 
 from notify_mtr import send
 from utils import get_data
@@ -17,40 +19,53 @@ class Smzdm:
         self.check_items = check_items
 
     @staticmethod
-    def sign(session):
+    def sign(cookie):
         try:
-            current = session.get(
-                url="https://zhiyou.smzdm.com/user/info/jsonp_get_current"
-            ).json()
-            if current["checkin"]["has_checkin"]:
-                msg = (
-                    f"用户信息: {current.get('nickname', '')}\n"
-                    f"目前积分: {current.get('point', '')}\n"
-                    f"经验值: {current.get('exp', '')}\n"
-                    f"金币: {current.get('gold', '')}\n"
-                    f"碎银子: {current.get('silver', '')}\n"
-                    f"威望: {current.get('prestige', '')}\n"
-                    f"等级: {current.get('level', '')}\n"
-                    f"已经签到: {current.get('checkin', {}).get('daily_checkin_num', '')} 天"
-                )
+            ts = int(round(time.time() * 1000))
+            url = 'https://user-api.smzdm.com/robot/token'
+            headers = {
+                'Host': 'user-api.smzdm.com',
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'Cookie': f'{cookie}',
+                'User-Agent': 'smzdm_android_V10.4.1 rv:841 (22021211RC;Android12;zh)smzdmapp',
+            }
+            data = {
+                "f": "android",
+                "v": "10.4.1",
+                "weixin": 1,
+                "time": ts,
+                "sign": hashlib.md5(bytes(f'f=android&time={ts}&v=10.4.1&weixin=1&key=apr1$AwP!wRRT$gJ/q.X24poeBInlUJC', encoding='utf-8')).hexdigest().upper()
+            }
+            html = requests.post(url=url, headers=headers, data=data)
+            res = html.json()
+            token = res['data']['token']
+
+            Timestamp = int(round(time.time() * 1000))
+            data = {
+                "f": "android",
+                "v": "10.4.1",
+                "sk": "ierkM0OZZbsuBKLoAgQ6OJneLMXBQXmzX+LXkNTuKch8Ui2jGlahuFyWIzBiDq/L",
+                "weixin": 1,
+                "time": Timestamp,
+                "token": token,
+                "sign": hashlib.md5(bytes(f'f=android&sk=ierkM0OZZbsuBKLoAgQ6OJneLMXBQXmzX+LXkNTuKch8Ui2jGlahuFyWIzBiDq/L&time={Timestamp}&token={token}&v=10.4.1&weixin=1&key=apr1$AwP!wRRT$gJ/q.X24poeBInlUJC', encoding='utf-8')).hexdigest().upper()
+            }
+            url = 'https://user-api.smzdm.com/checkin'
+            url2 = 'https://user-api.smzdm.com/checkin/all_reward'
+            headers = {
+                'Host': 'user-api.smzdm.com',
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'Cookie': f'{cookie}',
+                'User-Agent': 'smzdm_android_V10.4.1 rv:841 (22021211RC;Android12;zh)smzdmapp',
+            }
+            html = requests.post(url=url, headers=headers, data=data)
+            html2 = requests.post(url=url2, headers=headers, data=data)
+            res = json.loads(html.text)
+            res2 = json.loads(html2.text)
+            if res2['error_code'] == '0':
+                msg = res2["title"] + res2["sub_title"]
             else:
-                data = (
-                    session.get(
-                        url="https://zhiyou.smzdm.com/user/checkin/jsonp_checkin"
-                    )
-                    .json()
-                    .get("data", {})
-                )
-                msg = (
-                    f"用户信息: {current.get('nickname', '')}\n"
-                    f"目前积分: {data.get('point', '')}\n"
-                    f"增加积分: {data.get('add_point', '')}\n"
-                    f"经验值: {data.get('exp', '')}\n"
-                    f"金币: {data.get('gold', '')}\n"
-                    f"威望: {data.get('prestige', '')}\n"
-                    f"等级: {data.get('rank', '')}\n"
-                    f"已经签到: {data.get('checkin_num', {})} 天"
-                )
+                msg = res['error_msg']
         except Exception as e:
             msg = f"签到状态: 签到失败\n错误信息: {e}，请重新获取 cookie"
         return msg
@@ -59,29 +74,7 @@ class Smzdm:
         msg_all = ""
 
         for check_item in self.check_items:
-            cookie = {
-                item.split("=")[0]: quote(unquote(item.split("=")[1]))
-                for item in check_item.get("cookie").split("; ")
-                if item.split("=")[0] == "sess"
-            }
-            session = requests.session()
-            session.cookies.update(cookie)
-            session.headers.update(
-                {
-                    "Accept": "*/*",
-                    "Accept-Encoding": "gzip, deflate, br",
-                    "Accept-Language": "zh-CN,zh;q=0.9",
-                    "Connection": "keep-alive",
-                    "Host": "zhiyou.smzdm.com",
-                    "Referer": "https://www.smzdm.com/",
-                    "Sec-Fetch-Dest": "script",
-                    "Sec-Fetch-Mode": "no-cors",
-                    "Sec-Fetch-Site": "same-site",
-                    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
-                    "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.131 Safari/537.36",
-                }
-            )
-            msg = self.sign(session)
+            msg = self.sign(check_item.get("cookie"))
             msg_all += msg + "\n\n"
         return msg_all
 
@@ -90,4 +83,4 @@ if __name__ == "__main__":
     _data = get_data()
     _check_items = _data.get("SMZDM", [])
     result = Smzdm(check_items=_check_items).main()
-    send("什么值得买", result)
+    send("什么值得买APP", result)
